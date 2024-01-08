3ad84a6c0c28421c53b3c42f79d9866f9f71ea95
TAWSIF AHMED <stanfordblaze4@gmail.com>
2024-01-09 03:36:18 +0600

    intial commit

---
+++ CMakeFiles/world.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/world.dir/hello_world.cpp.obj"
+  "CMakeFiles/world.dir/hello_world.cpp.obj.d"
+  "libworld.dll.a"
+  "world.exe"
+  "world.exe.manifest"
+  "world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "C:/src/vcpkg/packages/raylib_x64-windows/include/raylib.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
